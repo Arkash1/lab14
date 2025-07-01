c67a87210b85b0944c33b8cdea398b705c54d728
Ara Kharatyan <92663086+Arkash1@users.noreply.github.com>
2025-07-01 15:00:07 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,31 +1,17 @@
-package com.company;
-
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите строку: ");
-        String input = scanner.nextLine();
+        System.out.println("Введите ip-адреса через пробел: ");
+        Scanner in = new Scanner(System.in);
+        String ips = in.nextLine();
+        String[] Ips = ips.split(" ");
+        for (String ip: Ips) {
+            if (ip.matches("(\\b([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.){3}([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\b")){
+                System.out.println("Корректный ip-адрес: " + ip);
+            }
+            else System.out.println("Не корректный ip-адрес: " + ip);
 
-        
-        String ipRegex = "\\b((25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)\\.){3}"
-                + "(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)\\b";
-
-        Pattern pattern = Pattern.compile(ipRegex);
-        Matcher matcher = pattern.matcher(input);
-
-        boolean found = false;
-        System.out.println("Найденные IP-адреса:");
-        while (matcher.find()) {
-            System.out.println(matcher.group());
-            found = true;
-        }
-
-        if (!found) {
-            System.out.println("Корректных IP-адресов не найдено.");
         }
     }
 }
